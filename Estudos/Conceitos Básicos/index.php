834b35c02490d3f5bb9d030e5744ace645c05ff8
Bernardo Paz de Freitas <125703112+b3rnard0p@users.noreply.github.com>
2025-03-30 01:16:05 -0300

    Add files via upload

---
+++ Estudos/Conceitos Básicos/index.php
@@ -0,0 +1,130 @@
+<?php
+//Exibir dados
+
+// echo 'Bernardo';
+
+// print 'Bernardo';
+
+// var_dump('Bernardo');
+
+//Variáveis
+
+// $nome = 'Bernardo';
+// echo $nome;
+
+// $idade = 20;
+// $valor = 10.5;
+// $booleano = true;
+
+// var_dump($idade);
+// var_dump($valor);
+// var_dump($booleano);
+
+//Concatenar
+
+// echo $nome . 'idade: ' . $idade;
+
+//Operadores aritméticos
+
+// $a = 10;
+// $b = 20;
+
+// echo $a + $b;
+// echo $a - $b;
+// echo $a * $b;
+// echo $a / $b;
+
+//Operadores de comparação
+
+// $a = 10;
+// $b = 20;
+
+// var_dump($a == $b); //igual
+// var_dump($a === $b); //idêntico
+// var_dump($a != $b); //diferente
+// var_dump($a !== $b); //não idêntico
+// var_dump($a > $b); //maior
+// var_dump($a < $b); //menor
+// var_dump($a >= $b); //maior ou igual
+// var_dump($a <= $b); //menor ou igual
+
+//Operadores lógicos
+
+// $a = true;
+// $b = false;
+// var_dump($a && $b); //e
+// var_dump($a || $b); //ou
+// var_dump(!$a); //não
+// var_dump(!$b); //não
+
+//Funcões
+
+//function saudacao ($nome) {
+//     return 'Olá ' . $nome;
+// }
+
+// echo saudacao('Bernardo');
+
+//Estruturas de controle
+
+// $idade = 18;
+
+// if ($idade >= 18) {
+//      echo 'Maior de idade';
+//  } else {
+//      echo 'Menor de idade';
+//  }
+
+// $idade = 18;
+
+// switch ($idade) {
+//     case 18:
+//         echo 'Você tem 18 anos';
+//         break;
+//     case 20:
+//         echo 'Você tem 20 anos';
+//         break;
+//     default:
+//         echo 'Você não tem 18 ou 20 anos';
+//         break;
+// }   
+
+//Laços de repetição
+
+//  for ($i = 0; $i < 10; $i++) {
+//      echo $i . '<br>';
+//  }
+//  $i = 0;
+
+//  while ($i < 10) {
+//      echo $i . '<br>';
+//      $i++;
+//  }
+
+//Arrays
+//  $nomes = ['Bernardo', 'Lucas', 'João'];
+
+//  print_r($nomes);
+
+//Foreach
+//  foreach ($nomes as $nome) {
+//      echo $nome . '<br>';
+//  }
+
+//Orientação a objetos
+//  class Carro {
+//     //Atributos
+//      public $cor;
+//      public $modelo;
+
+//      public function saudacao() {
+//          return 'Olá, eu sou um carro';
+//      }
+//  }
+
+//  $carro = new Carro();
+//  $carro->cor = 'azul';
+//  $carro->modelo = 'Fusca';
+// echo $carro->saudacao() . '<br>';
+
+?>
